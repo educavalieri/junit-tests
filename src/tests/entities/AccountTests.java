304fd748146a3528a1d6e15e9b79df32983c8027
educavalieri <eduardocavalieri@hotmail.com>
2022-01-28 20:07:53 -0300

    COMMENTS AAA

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -8,12 +8,17 @@ public class AccountTests {
 
     @Test
     public void depositShouldIncreaseBalanceWhenPositiveAmount(){
+        //AAA
+
+        //ARRANGE
         double amount = 200.0;
         double expectedValue = 196.0;
 
+        //ACT
         Account account = new Account(1L,0.0);
         account.deposit(amount);
 
+        //ASSERT
         AssertJUnit.assertEquals(expectedValue, account.getBalance());
 
     }
