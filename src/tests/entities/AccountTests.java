4c33c5d7c1ac98b63b7ecd76dcc2c9fc119228db
educavalieri <eduardocavalieri@hotmail.com>
2022-01-28 22:55:35 -0300

    include FactoryAccount and new automat tests

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -2,7 +2,9 @@ package tests.entities;
 import entities.Account;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
+import org.testng.annotations.TestInstance;
 import org.testng.asserts.Assertion;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
@@ -21,6 +23,77 @@ public class AccountTests {
         //ASSERT
         AssertJUnit.assertEquals(expectedValue, account.getBalance());
 
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount(){
+
+        //ARRANGE
+        double expectedValue = 100.0;
+        Account account = new Account(1L, expectedValue);
+        double amount = -200.0;
+
+        //ACT
+        account.deposit(amount);
+
+        //ASSERT
+        AssertJUnit.assertEquals(expectedValue, account.getBalance());
+    }
+
+    @Test
+    public void factoryDepositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount(){
+        //Arrange
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account account = AccountFactory.createEmptyAccount();
+
+        //Act
+        account.deposit(amount);
+
+        //Assert
+        AssertJUnit.assertEquals(expectedValue, account.getBalance());
+    }
+
+    @Test
+    public void factoryDepositShouldDoNothingWhenNegativeAmount() {
+        //Arrange
+        double expectedValue = 200.0;
+        double amount = -200.0;
+        Account account = AccountFactory.createAccount(200.0);
+
+        //Act
+        account.deposit(amount);
+
+        //Assert
+        AssertJUnit.assertEquals(expectedValue, account.getBalance());
+    }
+
+    @Test
+    public void fullWithdrawShouldRescuelAll(){
+        //Arrange
+        double expectedValue = 0.0;
+        double actualBalance = 200.0;
+
+        //Act
+        Account account = AccountFactory.createAccount(actualBalance);
+        double rescue = account.fullWithdraw();
+
+        //Assert
+        AssertJUnit.assertEquals(actualBalance, rescue);
+    }
+
+    @Test
+    public void fullWithdrawShouldClearBalance(){
+        //Arrange
+        double balance = 200.0;
+        double finalBalance = 0.0;
+
+        //Act
+        Account account = AccountFactory.createAccount(balance);
+        account.fullWithdraw();
+
+        //Assert
+        AssertJUnit.assertTrue(finalBalance == account.getBalance());
     }
 
 }
